879580d702668433f7cf1f83c517ebe8b4c8f95c
PAU <edsheeph>
2019-05-03 17:55:17 +0800

    Initial Commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        // return view('pages.index');
+        $title = 'Home Page';
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        // return view('pages.about');
+        $title = 'About Page';
+        return view('pages.about')->with('title', $title);
+    }
+
+    public function services(){
+        $data = array(
+            'title' => 'Services Page',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+
+}
